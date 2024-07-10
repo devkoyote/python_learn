996572e486262ecb9ff77dbf417e909c783aa0cd
devKoyote <alencar.gestao@gmail.com>
2024-07-10 20:59:27 -0300

    initial pythonLearn

---
+++ main.py
@@ -0,0 +1,41 @@
+# Variables and commentaries
+name_inter = "Daniel Sam"
+
+type_int = 11
+
+print(type(name_inter))
+print(type(type_int))
+
+# CONSTANTS
+
+VARIABLE_CONTANTE_NAME = "Daniel Alencar"
+print(VARIABLE_CONTANTE_NAME)
+VARIABLE_CONTANTE_NAME = "Alterada pq não existe, ah boas praticas"
+print(VARIABLE_CONTANTE_NAME)
+
+# Booleans first character Uppercase
+
+var_true = True
+var_false = False
+
+# Float
+
+var_float = 5.5
+
+# Operation mathematics
+
+print(4 + 5)
+print(6  - 7)
+print(3 * 9)
+print( 8 / 4)
+
+# Use value var type_int and multiple
+print(type_int * 3)
+
+# Comparison operators
+print(4 > 5)
+print(5 == 5)
+print(6 < 7)
+print(4 <= 7)
+print(9 >= 4)
+print(5 != 5)
