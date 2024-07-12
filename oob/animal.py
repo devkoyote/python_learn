dc8c9271bc02a4625823d087f703e1bbd1a757c7
devkoyote <alencar.gestao@gmail.com>
2024-07-12 17:20:18 -0300

    visibility and encapsulation

--- oob/animal.py
+++ oob/animal.py
@@ -2,6 +2,30 @@
 class Animal:
 
     def __init__(self, nome, idade, cor):
-        self.nome = nome
-        self.idade = idade
-        self.cor = cor
+        self._nome = nome
+        self._idade = idade
+        self._cor = cor
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    @property
+    def idade(self):
+        return self._idade
+
+    @idade.setter
+    def idade(self, idade):
+        self._idade = idade
+
+    @property
+    def cor(self):
+        return self._cor
+
+    @cor.setter
+    def cor(self, cor):
+        self._cor = cor
--- oob/cachorro.py
+++ oob/cachorro.py
@@ -2,9 +2,21 @@ from animal import Animal
 
 class Cachorro(Animal):
     def __init__(self, nome, idade, cor, qtd_ossos):
-        self.qtd_ossos = qtd_ossos
+        self.__qtd_ossos = qtd_ossos
         # Extends generic class
         super().__init__(nome, idade, cor)
+
+
+    # Methods Access [GET | SETTER ]
+    @property
+    def qtd_ossos(self):
+        return self.__qtd_ossos
+
+    @qtd_ossos.setter
+    def qtd_ossos(self, qtd_ossos):
+        self.__qtd_ossos = qtd_ossos
+
+
         # Function action
     def latir(self):
         return "Au au"
